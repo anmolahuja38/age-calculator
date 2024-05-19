7a976a5ce4fa0f79f78dfa83ca5d80675458e8b9
anmolahuja38 <70136141+anmolahuja38@users.noreply.github.com>
2024-05-19 13:25:13 +0530

    Google Ad Sense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import { AgeCalculator } from './AgeCalculator';
+import { AdsComponent } from './AdsComponent';
 
 function App() {
   return (
     <div className="App">
       <AgeCalculator></AgeCalculator>
+      <h1>Place To show Google AdSense</h1>
+      <AdsComponent dataAdSlot='6734630747' />
+
     </div>
   );
 }
