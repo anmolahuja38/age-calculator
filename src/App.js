e9e0fed47ec9e0b4d2943d81d0586e573a27278e
anmolahuja38 <70136141+anmolahuja38@users.noreply.github.com>
2024-05-19 13:13:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { AgeCalculator } from './AgeCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator></AgeCalculator>
+    </div>
+  );
+}
+
+export default App;
